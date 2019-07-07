52ca7f517608184fa5befc1a817e909ee1e1402c
Elli Steck <petersen.elli@gmail.com>
2019-07-07 14:07:37 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# pbp-cbox-notifications
-A ProBoards v5 plugin that hides a Cbox behind a dialog window, and shows a notification on the trigger button when a new message has been posted.
+# Cbox Notifications
+A plugin built with the free forum software [ProBoards](https://proboards.com/) that hides a Cbox behind a dialog window, and shows a notification on the trigger button when a new message has been posted.
+
+To test that the plugin is working correctly: duplicate your browser tab, open the dialog from one tab, and send a message. You should see the notification appear over the trigger button in the other tab.
+
+## Installation
+1. Download the [cbox-notifications.pbp](cbox-notifications.pbp) file and save it somewhere easily accessible, such as your Desktop or Downloads folder
+2. Go to your forum's Admin Panel
+3. Click on **Plugins > Manage**
+4. Click the "Import Plugin" button
+5. Click the "Choose Plugin File" button
+6. Navigate to and select the cbox-notifications.pbp file
+7. Once the plugin has been imported, click the "Finished" button near the bottom right of the modal dialog window
+
+**NOTE:**
+> You will be required to fill out the `cbox_url` field with your Cbox's unique URL before the plugin can be enabled.
+
+## Acknowledgements
+Special thanks to [Brian Ordonez](http://support.proboards.com/user/33409), [Peter](http://support.proboards.com/user/2671), and [Chris](http://support.proboards.com/user/65613) for their help with getting me started on this plugin.
---
+++ src/cbox-notifications.js
@@ -0,0 +1,192 @@
+/*!
+ * Plugin Name: Cbox Notifications
+ * Plugin URI:  http://ellitest.proboards.com
+ * Author:      Elli
+ * Author URI:  http://ellimccale.com/
+ * Version:     0.0.1
+ */
+
+(function () {
+
+    var PLUGIN_ID = 'cbox_notifications';
+    var ELEMENT_ID = 'cbox-notifications';
+    var DIALOG_ID = 'js-' + ELEMENT_ID + '-dialog';
+
+    var settings = {};
+
+    var hasNewMessage = false;
+    var dialogIsOpen = false;
+
+    var $cboxButton;
+    var $cboxNotification = $('<span class="' + ELEMENT_ID + '-new"><span class="visually-hidden">New Message</span></span>');
+
+    function _init() {
+
+        _getSettings();
+
+        $(_onDocumentReady);
+
+    }
+
+    function _getSettings() {
+
+        var plugin = pb.plugin.get(PLUGIN_ID);
+
+        if (plugin && plugin.settings) {
+            settings = plugin.settings;
+        }
+
+    }
+
+    function _onDocumentReady() {
+
+        _buildDialog();
+        _buildButton();
+        _fetchCboxData();
+        _fetchStorageData();
+
+    }
+
+    function _buildDialog() {
+
+        var isDialogDraggable = (settings.dialog_draggable === 'true');
+
+        pb.window.dialog(DIALOG_ID, {
+
+            autoOpen: false,
+            draggable: isDialogDraggable,
+            modal: true,
+            resizable: false,
+            title: settings.dialog_title,
+            width: settings.cbox_width,
+
+            open: function (e, ui) {
+                console.log('The dialog should be open.');
+                dialogIsOpen = true;
+            },
+
+            close: function (e, ui) {
+                console.log('The dialog should be closed.');
+                dialogIsOpen = false;
+            }
+
+        });
+
+        $('#' + DIALOG_ID).html(_buildDialogContent);
+
+    }
+
+    function _buildDialogContent() {
+
+        return $('<iframe>').attr({
+
+            allowtransparency: 'yes',
+            frameborder: '0',
+            height: parseInt(settings.cbox_height),
+            marginheight: '0',
+            marginwidth: '0',
+            scrolling: 'auto',
+            src: settings.cbox_url,
+            width: '100%',
+
+        });
+
+    }
+
+    function _buildButton() {
+
+        $cboxButton = $('<button>').attr({
+            id: 'js-' + ELEMENT_ID + '-button',
+            class: ELEMENT_ID + '-button',
+            type: 'button',
+        });
+
+        $cboxButton.html(
+            '<i class="fas fa-comment" aria-hidden="true"></i>' +
+            '<span class="visually-hidden">Show Chat, opens a dialog window</span>'
+        );
+
+        $cboxButton.on('click', function() {
+            _removeNotification();
+            $('#' + DIALOG_ID).dialog('open');
+        });
+
+        $('body').append($cboxButton);
+
+    }
+
+    function _fetchCboxData() {
+
+        window.addEventListener('message', function (e) {
+
+            if (!e || !e.data) {
+                return;
+            }
+
+            if (!e.origin.match(/cbox.ws$/)) {
+                return;
+            }
+
+            var cboxdata = {};
+
+            try {
+                cboxdata = JSON.parse(e.data);
+            } catch (e) {
+                return;
+            }
+
+            var event = cboxdata['event'];
+
+            if (event === 'message' && !dialogIsOpen) {
+
+                _addNotification();
+
+                localStorage.setItem('hasNewMessage', 'true');
+
+                console.log('New message received from Cbox!');
+
+                console.log('Notification and localStorage item were added! The dialog should be closed.');
+
+            }
+
+        });
+
+    }
+
+    function _fetchStorageData() {
+
+        if (localStorage.getItem('hasNewMessage') === 'true') {
+            _addNotification();
+            console.log('localStorage remembered to add a notification. The dialog should be closed.');
+        }
+
+    }
+
+    function _addNotification() {
+
+        if (!hasNewMessage) {
+            hasNewMessage = true;
+            $cboxButton.prepend($cboxNotification);
+        }
+
+    }
+
+    function _removeNotification() {
+
+        if (hasNewMessage) {
+
+            hasNewMessage = false;
+
+            $cboxNotification.remove();
+
+            localStorage.removeItem('hasNewMessage');
+
+            console.log('Notification and localStorage item were removed! Dialog should be open.');
+
+        }
+
+    }
+
+    _init();
+
+})();
